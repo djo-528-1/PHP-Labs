08106a2aef898f7b4f99f03e4e469126228dd991
djo_528_1 <verizhnikoveg@gmail.com>
2024-11-09 14:36:47 +0300

    3.2

---
+++ lab3/site/inc/data.inc.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+$rows = 7;
+$cols = 7;
+$color = 'red';
+$menu = [
+    ['link' => 'Домой', 'href' => 'index.php',],
+    ['link' => 'О нас', 'href' => 'about.php',],
+    ['link' => 'Контакты', 'href' => 'contact.php',],
+    ['link' => 'Таблица умножения', 'href' => 'table.php',],
+    ['link' => 'Калькулятор', 'href' => 'calc.php',]];
